2c648e6466ca7d36c111244c50d2edaf3d401f51
qrbaxter <qrbaxter90@gmail.com>
2023-03-12 16:46:15 +1100

    CommitMessg

--- src/Context.js
+++ src/Context.js
@@ -3,9 +3,29 @@ import React, {useState, useEffect} from "react"
 const Context = React.createContext()
 
 function ContextProvider({children}){
- 
+    
+    const [allCards, setAllCards] = useState([])
+
+    
+    const url = "https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/"
+    useEffect(() => {
+
+        fetch(url)
+          .then((response) => response.json())
+          .then((data) =>
+            fetch(
+              `https://apis.scrimba.com/deckofcards/api/deck/${data.deck_id}/draw/?count=20`
+            )
+          )
+          .then((response) => response.json())
+          .then((data) => setAllCards(data.cards))
+          .catch((error) => console.log(error));
+      }, []);
+
     return (
-      <Context.Provider value = {{}}>
+      <Context.Provider value = {{
+        allCards, setAllCards
+      }}>
         {children}
       </Context.Provider>
     )
